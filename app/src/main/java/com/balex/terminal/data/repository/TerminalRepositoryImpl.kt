d5dc94cff008100195717194c32fbcba2e4c87f3
balex <balexvicx@gmail.com>
2024-05-31 18:16:13 +0200

    added init size to terminalState

--- app/src/main/java/com/balex/terminal/data/repository/TerminalRepositoryImpl.kt
+++ app/src/main/java/com/balex/terminal/data/repository/TerminalRepositoryImpl.kt
@@ -30,10 +30,10 @@ class TerminalRepositoryImpl @Inject constructor(
         } else {
             _quotesListAndFrame = _quotesListAndFrameLastState
         }
-        isQuotesListNeedRefreshFlow.tryEmit(Unit)
-//            coroutineScope.launch() {
-//                isQuotesListNeedRefreshFlow.emit(Unit)
-//            }
+        //isQuotesListNeedRefreshFlow.tryEmit(Unit)
+            coroutineScope.launch() {
+                isQuotesListNeedRefreshFlow.emit(Unit)
+            }
     }
 
 
